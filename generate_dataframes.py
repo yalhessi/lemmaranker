6066387baeb589f4f6e9a1a0ef5e3e9fa26df99f
Yousef Alhessi <yalhessi@gmail.com>
2022-08-02 17:51:03 -0600

    add proof progress features

--- generate_dataframes.py
+++ generate_dataframes.py
@@ -2,6 +2,7 @@ import utils
 import log_parser
 import pandas as pd
 import random 
+import sexpdata
 from multiprocessing import Pool, set_start_method
 set_start_method('spawn', True)
 
@@ -23,18 +24,29 @@ def add_alpha(frame, index):
 def add_lemma_len(frame, index):
   frame.at[index, "lemma_len"] = len(frame.at[index, "lemma"])
   frame.at[index, "ugly_lemma_len"] = len(frame.at[index, "ugly_lemma"])
+  frame.at[index, "sexp_lemma_size"] = utils.sexp_size(sexpdata.loads(f"({utils.get_goal(frame.at[index, 'ugly_lemma'])})"))
+  frame.at[index, "norm_lemma_len"] = frame.at[index, "lemma_len"] / len(frame.at[index, 'stuck_state'])
+  frame.at[index, "norm_ugly_lemma_len"] = frame.at[index, "ugly_lemma_len"] / len(frame.at[index, 'stuck_state'])
+  frame.at[index, "norm_sexp_lemma_size"] = frame.at[index, "sexp_lemma_size"] / len(frame.at[index, 'stuck_state'])
 
 def remove_notations(frame, index):
   frame.at[index, "ugly_lemma"] = utils.disable_notations(frame.at[index, "prelude"], frame.at[index, "prefix"], frame.at[index, "lemma"])
   frame.at[index, "ugly_theorem"] = utils.disable_notations(frame.at[index, "prelude"], frame.at[index, "prefix"], frame.at[index, "theorem"])
   frame.at[index, "ugly_helper_lemma"] = utils.disable_notations(frame.at[index, "prelude"], frame.at[index, "prefix"], frame.at[index, "helper_lemma"])
 
+def add_edit_distance(frame, index):
+  frame.at[index, "ted"] = utils.ted(f"({utils.get_goal(frame.at[index, 'ugly_theorem'])})", f"({utils.get_goal(frame.at[index, 'ugly_lemma'])})")
+  frame.at[index, "sed"] = utils.sed(utils.get_goal(frame.at[index, 'ugly_theorem']), utils.get_goal(frame.at[index, 'ugly_lemma']))
 
-def add_ted(frame, index):
-  frame.at[index, "ted"] = 0
+def add_IH_use(frame, index):
+  frame.at[index, "use_IH_before"] = utils.uses_IHs_before(frame, index)
+  frame.at[index, "can_use_IH_before"] = utils.can_use_IH_before(frame, index)
+  frame.at[index, "can_use_IH_after"] = utils.can_use_IH_after(frame, index)
 
-def add_sed(frame, index):
-  frame.at[index, "sed"] = 0
+def add_synth_use(frame, index):
+  frame.at[index, "simpl_before"] = utils.can_simpl_before(frame, index)
+  frame.at[index, "can_use_synth"] = utils.can_use_synth(frame, index)
+  frame.at[index, "simpl_after"] = utils.can_simpl_after(frame, index)
 
 def add_column(frame, col_fun):
   for r, row in frame.iterrows():
@@ -57,8 +69,7 @@ def file_to_dataframe(filedata):
       add_is_weaker(frame, r)
       add_is_stronger(frame, r)
       add_is_equiv(frame, r)
-      add_sed(frame, r)
-      add_ted(frame, r)
+      add_edit_distance(frame, r)
       # pass
     except Exception as e:
       print(e)
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
-
-from numpy import isin
 import coq_serapy
+import re
+import sexpdata
+import zss
 
 # def is_trivial(prelude, prefix, lemma):
 #     if isinstance(prefix, str):
@@ -90,3 +91,222 @@ def disable_notations(prelude, prefix, theorem):
         coq.run_stmt('Unset Printing Notations.')
         coq.run_stmt(theorem)
         return coq.goals
+
+def can_use_tactic(prelude, prefix, theorem, proof_cmds, tactic):
+  with coq_serapy.SerapiContext(
+          ["sertop", "--implicit"],    
+          None,
+          prelude) as coq:
+    for stmt in prefix:
+        coq.run_stmt(stmt)
+    coq.run_stmt(theorem)
+    for cmd in proof_cmds:
+        coq.run_stmt(cmd)
+    try:
+      coq.run_stmt(tactic)
+      return True
+    except:
+      return False  
+
+def can_use_helper(prelude, prefix, theorem, proof_cmds, helper):
+  return can_use_tactic(prelude, prefix, theorem, proof_cmds, f'apply {helper} || rewrite {helper} || rewrite <- {helper} || fail.')
+
+def can_use_IH_before(frame, index):
+  prelude = frame.at[index, 'prelude']
+  prefix = coq_serapy.read_commands(frame.at[index, 'prefix'])
+  helper_lemma = frame.at[index, 'helper_lemma']
+  helper_lemma_proof = coq_serapy.read_commands(frame.at[index, 'helper_lemma_proof'].replace('.', '. '))
+  theorem = frame.at[index, 'theorem']
+  proof_cmds_before, _ = split_theorem_proof(frame, index)
+  IHs = get_IHs(frame, index)
+  return any(can_use_helper(prelude, prefix + [helper_lemma] + helper_lemma_proof, theorem, proof_cmds_before, ih) for ih in IHs)
+
+def can_use_IH_after(frame, index):
+  if can_use_synth(frame, index):
+    prelude = frame.at[index, 'prelude']
+    prefix = coq_serapy.read_commands(frame.at[index, 'prefix'])
+    helper_lemma = frame.at[index, 'helper_lemma']
+    helper_lemma_proof = coq_serapy.read_commands(frame.at[index, 'helper_lemma_proof'].replace('.', '. '))
+    synth_lemma = frame.at[index, 'lemma']
+    synth_lemma_name = frame.at[index, 'lemma_name']
+    theorem = frame.at[index, 'theorem']
+    proof_cmds_before, _ = split_theorem_proof(frame, index)
+    IHs = get_IHs(frame, index)
+    return any(can_use_helper(prelude, 
+                          prefix + [helper_lemma] + helper_lemma_proof + [synth_lemma, 'Admitted.'], 
+                          theorem, 
+                          proof_cmds_before + [f'apply {synth_lemma_name} || rewrite {synth_lemma_name} || rewrite <- {synth_lemma_name} || fail.'], 
+                          ih) for ih in IHs)
+  else:
+    return False
+
+def can_use_synth(frame, index):
+  prelude = frame.at[index, 'prelude']
+  prefix = coq_serapy.read_commands(frame.at[index, 'prefix'])
+  helper_lemma = frame.at[index, 'helper_lemma']
+  helper_lemma_proof = coq_serapy.read_commands(frame.at[index, 'helper_lemma_proof'].replace('.', '. '))
+  synth_lemma = frame.at[index, 'lemma']
+  synth_lemma_name = frame.at[index, 'lemma_name']
+  theorem = frame.at[index, 'theorem']
+  proof_cmds_before, _ = split_theorem_proof(frame, index)
+  return can_use_helper(prelude, prefix + [helper_lemma] + helper_lemma_proof + [synth_lemma, 'Admitted.'], theorem, proof_cmds_before, synth_lemma_name)
+
+def can_simpl_before(frame, index):
+  prelude = frame.at[index, 'prelude']
+  prefix = coq_serapy.read_commands(frame.at[index, 'prefix'])
+  helper_lemma = frame.at[index, 'helper_lemma']
+  helper_lemma_proof = coq_serapy.read_commands(frame.at[index, 'helper_lemma_proof'].replace('.', '. '))
+  theorem = frame.at[index, 'theorem']
+  proof_cmds_before, _ = split_theorem_proof(frame, index)
+  return can_use_tactic(prelude, prefix + [helper_lemma] + helper_lemma_proof, theorem, proof_cmds_before, 'simpl || fail.')
+
+def can_simpl_after(frame, index):
+  if can_use_synth(frame, index):
+    prelude = frame.at[index, 'prelude']
+    prefix = coq_serapy.read_commands(frame.at[index, 'prefix'])
+    helper_lemma = frame.at[index, 'helper_lemma']
+    helper_lemma_proof = coq_serapy.read_commands(frame.at[index, 'helper_lemma_proof'].replace('.', '. '))
+    synth_lemma = frame.at[index, 'lemma']
+    synth_lemma_name = frame.at[index, 'lemma_name']
+    theorem = frame.at[index, 'theorem']
+    proof_cmds_before, _ = split_theorem_proof(frame, index)
+    return can_use_tactic(prelude, 
+                          prefix + [helper_lemma] + helper_lemma_proof + [synth_lemma, 'Admitted.'], 
+                          theorem, 
+                          proof_cmds_before + [f'apply {synth_lemma_name} || rewrite {synth_lemma_name} || rewrite <- {synth_lemma_name} || fail.'], 
+                          'simpl || fail.')
+  else:
+    return False
+
+def uses_IHs_before(frame, index):
+  IHs = get_IHs(frame, index)
+  print(IHs)
+  cmds_before, _ = split_theorem_proof(frame, index)
+  print(cmds_before)
+  for cmd in cmds_before:
+    for ih in IHs:
+      if ih in cmd:
+        return True
+  return False
+
+def get_IHs(frame, index):
+  prelude = frame.at[index, 'prelude']
+  prefix = coq_serapy.read_commands(frame.at[index, 'prefix'])
+  helper_lemma = frame.at[index, 'helper_lemma']
+  helper_lemma_proof = coq_serapy.read_commands(frame.at[index, 'helper_lemma_proof'].replace('.', '. '))
+  theorem = frame.at[index, 'theorem']
+  proof_cmds_before, _ = split_theorem_proof(frame, index)
+
+  with coq_serapy.SerapiContext(
+          ["sertop", "--implicit"],    
+          None,
+          prelude) as coq:
+    for stmt in prefix:
+        coq.run_stmt(stmt)
+    coq.run_stmt(helper_lemma)
+    for cmd in helper_lemma_proof:
+        coq.run_stmt(cmd)
+    coq.run_stmt(theorem)
+
+    for cmd in proof_cmds_before:
+        coq.run_stmt(cmd)
+    IHs = []
+    for hyp in coq.hypotheses:
+        if hyp.startswith('IH'):
+            IHs.append(hyp.split(":")[0].strip())
+    return IHs
+
+def split_theorem_proof(frame, index):
+  proof_cmds = coq_serapy.read_commands(frame.at[index, "theorem_proof"].replace('.', '. '))
+  proof_cmds_before = []
+  proof_cmds_after = []
+  found = False
+  for cmd in proof_cmds:
+    if 'lfind.' in cmd:
+      found = True
+      continue
+    if not found:
+      proof_cmds_before.append(cmd)
+    else:
+      proof_cmds_after.append(cmd)
+  return proof_cmds_before, proof_cmds_after
+
+def sexp_size(sexp):
+  if isinstance(sexp, list):
+    return sum(sexp_size(child) for child in sexp)
+  else:
+    return 1
+
+def sexp_to_tree(sexp):
+  if isinstance(sexp, list):
+    tree = zss.Node(sexp[0])
+    for i in range(1, len(sexp)):
+      tree.addkid(sexp_to_tree(sexp[i]))
+    return tree
+  else:
+    return zss.Node(sexp)
+
+def ted_distance(s1, s2):
+  return zss.distance(sexp_to_tree(sexpdata.loads(s1)), sexp_to_tree(sexpdata.loads(s2)), zss.Node.get_children, insert_cost, remove_cost, update_cost1)
+  return zss.simple_distance(sexp_to_tree(s1), sexp_to_tree(s2), zss.Node.get_children, zss.Node.get_label, my_label_distance)
+
+def insert_cost(n):
+  return depth(n)
+
+def remove_cost(n):
+  return depth(n)
+
+def update_cost1(n1, n2):
+  return my_label_distance(zss.Node.get_label(n1), zss.Node.get_label(n2)) + abs(depth(n1) - depth(n2))
+
+def update_cost2(n1, n2):
+  children1 = zss.Node.get_children(n1)
+  children2 = zss.Node.get_children(n2)
+  if not children1:
+    return depth(n2)
+  if not children2:
+    return depth(n1)
+  dist = my_label_distance(zss.Node.get_label(n1), zss.Node.get_label(n2))
+  if len(children1) > len(children2):
+    small = children2
+    big = children1
+  else:
+    small = children1
+    big = children2
+  for i in range(len(small)):
+    dist += update_cost2(big[i], small[i])
+  for i in range(len(small), len(big)):
+    dist += insert_cost(big[i])
+  return dist
+
+
+def my_label_distance(l1, l2):
+  if l1 == l2:
+    return 0
+  else:
+    return 1
+
+def depth(tree : zss.Node):
+  if not zss.Node.get_children(tree):
+    return 1
+  else:
+    return 1 + max(depth(child) for child in zss.Node.get_children(tree))
+
+def ted(t1, t2):
+  return ted_distance(t1, t2)
+
+def sed(s1, s2):
+  import editdistance
+  return editdistance.eval(s1, s2)
+
+def get_thereorem_body(s):
+  if match := re.match(f'(Lemma|Theorem) (\w+)[ ]?:(?P<body>.+)', s):
+    return match.groupdict()["body"].strip()
+  else:
+    return s
+
+def get_goal(s):
+  if match := re.match(f'(.*)(forall|exists) (.+),(?P<goal>[\w\W]+)', s):
+    return match.groupdict()["goal"].strip()
+  else:
+    return s
